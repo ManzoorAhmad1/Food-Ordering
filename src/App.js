03cbeb718ef9d9e05705f616d28656138f3630a9
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-07-06 02:50:53 -0700

    useSlector for validation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useSelector } from 'react-redux';
+
 
 function App() {
+  const validation = useSelector(state => state.validation.validation)
   return (
     <Layout>
-      <Cart />
+      {validation && <Cart />}
       <Products />
     </Layout>
   );
